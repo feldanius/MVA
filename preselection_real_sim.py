c2395275384bc5db5425fc3f20426025f50cd5b7
Felipe De Jesus Martínez Tapia <43442037+feldanius@users.noreply.github.com>
2025-03-17 12:14:57 -0600

    Update preselection_real_sim.py

--- preselection_real_sim.py
+++ preselection_real_sim.py
@@ -148,7 +148,8 @@ class RDFanalysis:
         df = df.Define("jj_m", "JetConstituentsUtils::InvariantMass(jets_p4[0], jets_p4[1])")
         
         df = df.Define("missingEnergy", "FCCAnalyses::missingEnergy(365., ReconstructedParticles)")
-        df = df.Define("missingEnergy_energy", "missingEnergy[0]")
+        df = df.Define("missingEnergy_energy", "FCCAnalyses::ReconstructedParticle::get_e(missingEnergy[0])")
+        #df = df.Define("missingEnergy_energy", "missingEnergy[0]")
         #df = df.Alias("missingEnergy_energy", "missingEnergy")
         #df = df.Define("missingEnergy_energy", "FCCAnalyses::ReconstructedParticle::get_e(missingEnergy)")
         #df = df.Alias("missingEnergy.energy", "missingEnergy_energy")
