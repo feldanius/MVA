73a9247856b227d03d777f45aca33b13d06f9b01
Felipe De Jesus Martínez Tapia <43442037+feldanius@users.noreply.github.com>
2025-03-17 18:29:23 -0600

    Update convert_pkl_to_root.py

--- convert_pkl_to_root.py
+++ convert_pkl_to_root.py
@@ -3,6 +3,11 @@ from addons.TMVAHelper.TMVAHelper import TMVAHelperXGB
 input_pkl = "outputs/FCCee/higgs/mva/test_2_pkl/bdt_model_example.pkl"
 output_root = "outputs/FCCee/higgs/mva/test_2_pkl/bdt_model_example.root"
 
-TMVAHelperXGB.convert_model_to_ROOT(input_pkl, output_root)
+# Instanciar la clase
+helper = TMVAHelperXGB()
+
+# Usar el método adecuado
+helper.run_inference(input_pkl, output_root)
 
 print(f"Conversión completada: {output_root}")
+
