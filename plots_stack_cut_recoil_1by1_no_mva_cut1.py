907c2756c0b6bcc988e3525a8fefd287e2338ddb
Felipe De Jesus Martínez Tapia <43442037+feldanius@users.noreply.github.com>
2025-04-25 15:55:18 -0600

    Update plots_stack_cut_recoil_1by1_no_mva_cut1.py

--- plots_stack_cut_recoil_1by1_no_mva_cut1.py
+++ plots_stack_cut_recoil_1by1_no_mva_cut1.py
@@ -59,7 +59,7 @@ hists["missingEnergy_energy_fixed"] = {
     "xmin": 130,
     "xmax": 265,
     "ymin": 0,
-    "ymax": 235000,
+    "ymax": 55000,
     "xtitle": "MET (GeV)",
     "ytitle": "Events",
 }
@@ -72,7 +72,7 @@ hists["missing_p_fixed"] = {
     "xmin": 0,
     "xmax": 170,
     "ymin": 0,
-    "ymax": 210000,
+    "ymax": 35000,
     "xtitle": "Missing_p (GeV)",
     "ytitle": "Events",
 }
@@ -85,7 +85,7 @@ hists["jj_m"] = {
     "xmin": 90,
     "xmax": 155,
     "ymin": 0,
-    "ymax": 220000,
+    "ymax": 85000,
     "xtitle": "m_{jj} (GeV)",
     "ytitle": "Events",
 }
@@ -98,7 +98,7 @@ hists["hbb_m"] = {
     "xmin": 90,
     "xmax": 160,
     "ymin": 0,
-    "ymax": 250000,
+    "ymax": 85000,
     "xtitle": "RecoBuilder_mass (GeV)",
     "ytitle": "Events ",
 }
@@ -111,7 +111,7 @@ hists["hbb_p"] = {
     "xmin": 0,
     "xmax": 95,
     "ymin": 0,
-    "ymax": 85000,
+    "ymax": 32000,
     "xtitle": "RecoBuilder_p (GeV)",
     "ytitle": "Events",
 }
@@ -121,10 +121,10 @@ hists["higgs_recoil_m"] = {
     "logy": False,
     "stack": False,
     "rebin": 2,
-    "xmin": 70,
+    "xmin": 160,
     "xmax": 280,
     "ymin": 0,
-    "ymax": 42000,
+    "ymax": 37000,
     "xtitle": "higgs_recoil_mass (GeV)",
     "ytitle": "Events",
 }
@@ -152,7 +152,7 @@ hists["cosTheta_miss"] = {
     "xmin": 0,
     "xmax": 1.0,
     "ymin": 0,
-    "ymax": 55000,
+    "ymax": 25000,
     "xtitle": "cosTheta_miss",
     "ytitle": "Events",
 }
